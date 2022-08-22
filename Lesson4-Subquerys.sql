7cf80c84e03ec0cf9a1c41a318a2836606d1665f
Aaron Jamet <aaronjametorgiles@gmail.com>
2022-08-22 22:39:46 +0200

    Lecciones 4 y 5

--- Lesson4-Subquerys.sql
+++ Lesson4-Subquerys.sql
@@ -69,3 +69,239 @@ FROM (SELECT s.name rep_name, r.name region_name, SUM(o.total_amt_usd) total_amt
       ON o.account_id = a.id
       GROUP BY 1, 2) subq1
 GROUP BY 1;
+
+Essentially, this is a JOIN of these two tables, where the region and amount match.
+
+SELECT t3.rep_name, t3.region_name, t3.total_amt
+FROM(SELECT region_name, MAX(total_amt) total_amt
+     FROM(SELECT s.name rep_name, r.name region_name, SUM(o.total_amt_usd) total_amt
+             FROM sales_reps s
+             JOIN accounts a
+             ON a.sales_rep_id = s.id
+             JOIN orders o
+             ON o.account_id = a.id
+             JOIN region r
+             ON r.id = s.region_id
+             GROUP BY 1, 2) t1
+     GROUP BY 1) t2
+JOIN (SELECT s.name rep_name, r.name region_name, SUM(o.total_amt_usd) total_amt
+     FROM sales_reps s
+     JOIN accounts a
+     ON a.sales_rep_id = s.id
+     JOIN orders o
+     ON o.account_id = a.id
+     JOIN region r
+     ON r.id = s.region_id
+     GROUP BY 1,2
+     ORDER BY 3 DESC) t3
+ON t3.region_name = t2.region_name AND t3.total_amt = t2.total_amt;
+/*
+ 2. For the region with the largest sales total_amt_usd, how many total orders were placed?
+*/
+SELECT r.name, COUNT(o.total) total_orders
+FROM region r
+JOIN sales_reps sr
+ON sr.region_id = r.id
+JOIN accounts a
+ON sr.id = a.sales_rep_id
+JOIN orders o
+ON o.account_id = a.id
+GROUP BY r.name
+HAVING SUM(o.total_amt_usd) = (
+	     SELECT MAX(total_amt_selled) max_selled
+       FROM (SELECT r.name, SUM(total_amt_usd) total_amt_selled
+             FROM region r
+             JOIN sales_reps sr
+             ON sr.region_id = r.id
+             JOIN accounts a
+             ON sr.id = a.sales_rep_id
+             JOIN orders o
+             ON o.account_id = a.id
+             GROUP BY r.name) subq);
+/*
+ 3. How many accounts had more total purchases than the account name which has bought the most standard_qty paper
+   throughout their lifetime as a customer?
+*/
+SELECT COUNT(*)
+FROM (SELECT a.name
+      FROM orders o
+      JOIN accounts a
+      ON a.id = o.account_id
+      GROUP BY 1
+      HAVING SUM(o.total) >
+          (SELECT total
+           FROM (SELECT a.name, SUM(o.standard_qty) total_std, SUM(o.total) total
+                FROM accounts a
+                JOIN orders o
+                ON o.account_id = a.id
+                GROUP BY 1
+                ORDER BY 2 DESC
+                LIMIT 1) inner_subq)
+	       ) counter_subq;
+
+3 accounts is the answer.
+/*
+ 4. For the customer that spent the most (in total over their lifetime as a customer) total_amt_usd, how many
+   web_events did they have for each channel?
+ */
+SELECT a.name, w.channel, COUNT(*) total_web_events
+FROM accounts a
+JOIN web_events w
+ON w.account_id = a.id
+  AND a.id = (SELECT id
+              FROM (SELECT a.id, a.name, MAX(o.total_amt_usd) total_max
+                    FROM accounts a
+                    JOIN orders o
+                    ON o.account_id = a.id
+                    GROUP BY 1, 2
+                    ORDER BY 3 DESC
+                    LIMIT 1) subq)
+GROUP BY 1, 2
+ORDER BY 3 DESC;
+/*
+ 5. What is the lifetime average amount spent in terms of total_amt_usd for the top 10 total spending accounts?
+*/
+SELECT AVG(max_totals_spent)
+FROM (SELECT a.id, a.name, SUM(o.total_amt_usd) max_totals_spent
+      FROM accounts a
+      JOIN orders o
+      ON o.account_id = a.id
+      GROUP BY 1, 2
+      ORDER BY 3 DESC
+      LIMIT 10) subq;
+/*
+ 6. What is the lifetime average amount spent in terms of total_amt_usd, including only the companies that spent
+   more per order, on average, than the average of all orders.
+*/
+SELECT AVG(avg_amt)
+FROM (SELECT o.account_id, AVG(o.total_amt_usd) avg_amt
+      FROM orders o
+      GROUP BY 1
+      HAVING AVG(o.total_amt_usd) > (SELECT AVG(o.total_amt_usd) avg_all
+                                     FROM orders o)) subq;
+
+
+### WITH -> forma un poco más limpia de escribir las subquerys
+/*
+ 1. Provide the name of the sales_rep in each region with the largest amount of total_amt_usd sales.
+*/
+WITH t1 AS(
+       SELECT s.name rep_name, r.name region_name, SUM(o.total_amt_usd) total_amt
+       FROM sales_reps s
+       JOIN accounts a
+       ON a.sales_rep_id = s.id
+       JOIN orders o
+       ON o.account_id = a.id
+       JOIN region r
+       ON r.id = s.region_id
+       GROUP BY 1,2
+       ORDER BY 3 DESC),
+ t2 AS(
+       SELECT region_name, MAX(total_amt) total_amt
+       FROM t1
+       GROUP BY 1)
+
+SELECT t1.rep_name, t1.region_name, t1.total_amt
+FROM t1
+JOIN t2
+ON t1.region_name = t2.region_name AND t1.total_amt = t2.total_amt;
+/*
+ 2. For the region with the largest sales total_amt_usd, how many total orders were placed?
+*/
+WITH t1 AS (
+       SELECT r.name reg_name, SUM(total_amt_usd) total_amt_selled
+        FROM region r
+        JOIN sales_reps sr
+        ON sr.region_id = r.id
+        JOIN accounts a
+        ON sr.id = a.sales_rep_id
+        JOIN orders o
+        ON o.account_id = a.id
+        GROUP BY r.name),
+     t2 AS (
+       SELECT MAX(total_amt_selled) max_selled
+       FROM t1)
+
+SELECT r.name, COUNT(o.total) total_orders
+FROM region r
+JOIN sales_reps sr
+ON sr.region_id = r.id
+JOIN accounts a
+ON sr.id = a.sales_rep_id
+JOIN orders o
+ON o.account_id = a.id
+GROUP BY r.name
+HAVING SUM(o.total_amt_usd) = (SELECT * FROM t2);
+/*
+ 3.How many accounts had more total purchases than the account name which has bought the most standard_qty paper
+   throughout their lifetime as a customer?
+*/
+WITH t1 AS (
+      SELECT a.name ac_name, SUM(o.standard_qty) total_std, SUM(o.total) total
+       FROM accounts a
+       JOIN orders o
+       ON o.account_id = a.id
+       GROUP BY 1
+       ORDER BY 2 DESC
+       LIMIT 1),
+    t2 AS (
+     SELECT a.name
+      FROM orders o
+      JOIN accounts a
+      ON a.id = o.account_id
+      GROUP BY 1
+      HAVING SUM (o.total) > (SELECT total FROM t1))
+
+SELECT COUNT(*)
+FROM t2;
+/*
+ 4. For the customer that spent the most (in total over their lifetime as a customer) total_amt_usd, how many
+   web_events did they have for each channel?
+*/
+WITH t1 AS (
+      SELECT a.id, a.name ac_name, MAX(o.total_amt_usd) total_max
+        FROM accounts a
+        JOIN orders o
+        ON o.account_id = a.id
+        GROUP BY 1, 2
+        ORDER BY 3 DESC
+        LIMIT 1)
+
+SELECT a.name, w.channel, COUNT(*) total_web_events
+FROM accounts a
+JOIN web_events w
+ON a.id = w.account_id AND a.id = (SELECT id FROM t1)
+GROUP BY 1, 2
+ORDER BY 3 DESC;
+/*
+ 5. What is the lifetime average amount spent in terms of total_amt_usd for the top 10 total spending accounts?
+*/
+WITH t1 AS (
+       SELECT a.id, a.name ac_name, SUM(o.total_amt_usd) max_totals_spent
+        FROM accounts a
+        JOIN orders o
+        ON o.account_id = a.id
+        GROUP BY 1, 2
+        ORDER BY 3 DESC
+        LIMIT 10)
+
+SELECT ac_name, AVG(max_totals_spent) avg_spent
+FROM t1
+GROUP BY 1
+ORDER BY 2 DESC;
+/*
+ 6. What is the lifetime average amount spent in terms of total_amt_usd, including only the companies that spent
+   more per order, on average, than the average of all orders.What is the lifetime average amount spent in terms of
+   total_amt_usd, including only the companies that spent more per order, on average, than the average of all orders.
+*/
+WITH t1 AS (
+          SELECT AVG(o.total_amt_usd) avg_all
+           FROM orders o),
+     t2 AS (
+          SELECT o.account_id, AVG(o.total_amt_usd) avg_amt
+           FROM orders o
+           GROUP BY 1
+           HAVING AVG(o.total_amt_usd) > (SELECT * FROM t1))
+
+SELECT AVG(avg_amt)
+FROM t2;
---
+++ Lesson5-SQLDataCleaning.sql
@@ -0,0 +1,139 @@
+### LEFT, RIGHT & LENGTH
+/*
+ First example
+*/
+SELECT first_name, last_name, phone_number
+       LEFT(phone_number, 3),
+       RIGHT(phone_number, 8),
+       RIGHT(phone_number, LENGTH(phone_number - 4)) AS phone_number_alt
+FROM customer_data
+/*
+ 1. In the accounts table, there is a column holding the website for each company. The last three digits specify what
+   type of web address they are using. A list of extensions (and pricing) is provided here. Pull these extensions and
+   provide how many of each website type exist in the accounts table.
+*/
+SELECT RIGHT(website, 4) web_ext, COUNT(*) ext_times_used
+FROM accounts
+GROUP BY 1
+ORDER BY 2 DESC;
+/*
+ 2. There is much debate about how much the name (or even the first letter of a company name) matters. Use the accounts
+   table to pull the first letter of each company name to see the distribution of company names that begin with each
+   letter (or number).
+*/
+SELECT LEFT(UPPER(name), 1) first_letter, COUNT(*) num_companies
+FROM accounts
+GROUP BY 1
+ORDER BY 2 DESC;
+/*
+ 3. Use the accounts table and a CASE statement to create two groups: one group of company names that start with a
+   number and a second group of those company names that start with a letter. What proportion of company names start
+   with a letter?
+*/
+SELECT SUM(num) nums, SUM(letter) letters
+FROM (SELECT name,
+             CASE WHEN LEFT(UPPER(name), 1) IN ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+                  THEN 1 ELSE 0 END as num,
+             CASE WHEN LEFT(UPPER(name), 1) IN ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+                  THEN 0 ELSE 1 END as letter
+      FROM accounts) t1;
+
+There are 350 company names that start with a letter and 1 that starts with a number. This gives a ratio of 350/351
+that are company names that start with a letter or 99.7%.
+/*
+ 4. Consider vowels as a, e, i, o, and u. What proportion of company names start with a vowel, and what percent start
+   with anything else?
+*/
+SELECT SUM(vowel) vowels, SUM(other_char) other_chars
+FROM (SELECT name,
+             CASE WHEN LEFT(UPPER(name), 1) IN ('A', 'E', 'I', 'O', 'U')
+                  THEN 1 ELSE 0 END as vowel,
+             CASE WHEN LEFT(UPPER(name), 1) IN ('A', 'E', 'I', 'O', 'U')
+                  THEN 0 ELSE 1 END as other_char
+      FROM accounts) t1;
+
+There are 80 company names that start with a vowel and 271 that start with other characters. Therefore 80/351 are vowels
+or 22.8%. Therefore, 77.2% of company names do not start with vowels.
+
+
+### POSITION, STRPOS & SUBSTR
+/*
+ First example
+*/
+SELECT first_name, last_name, city_state,
+       POSITION(',' IN city_state) AS comma_position,
+       STRPOS(city_state, ',') AS substr_comma_position,
+       LOWER(city_state) AS lowercase,
+       UPPER(city_state) AS uppercase,
+       LEFT(city_state, POSITION(',' IN city_state) - 1) AS city_state
+FROM customer_data;
+/*
+ 1. Use the accounts table to create first and last name columns that hold the first and last names for the primary_poc.
+*/
+SELECT LEFT(primary_poc, STRPOS(primary_poc, ' ') - 1) AS first_name,
+       RIGHT(primary_poc, LENGTH(primary_poc) -STRPOS(primary_poc, ' ')) AS last_name
+FROM accounts;
+/*
+ 2. Now see if you can do the same thing for every rep name in the sales_reps table. Again provide first and last name
+   columns.
+*/
+SELECT LEFT(name, STRPOS(name, ' ') - 1) AS first_name,
+       RIGHT(name, LENGTH(name) -STRPOS(name, ' ')) AS last_name
+FROM sales_reps;
+
+
+### CONCAT & ||
+/*
+ First example
+*/
+SELECT first_name, last_name,
+       CONCAT(first_name, ' ', last_name) AS full_name,
+       first_name || ' ' || last_name AS full_name_alt
+FROM customer_data
+/*
+ 1. Each company in the accounts table wants to create an email address for each primary_poc. The email address should
+   be the first name of the primary_poc . last name primary_poc @ company name .com
+*/
+WITH t1 AS (
+  SELECT name,
+         LEFT(primary_poc, STRPOS(primary_poc, ' ') - 1) AS first_name,
+         RIGHT(primary_poc, LENGTH(primary_poc) - (STRPOS(primary_poc, ' ')) AS last_name
+  FROM accounts
+)
+
+SELECT first_name, last_name,
+       CONCAT(first_name, '.', last_name, '@', name, '.com')
+FROM t1;
+/*
+ 2. You may have noticed that in the previous solution some of the company names include spaces, which will certainly
+   not work in an email address. See if you can create an email address that will work by removing all of the spaces
+   in the account name, but otherwise your solution should be just as in question 1.
+*/
+WITH t1 AS (
+  SELECT name,
+         LEFT(primary_poc, STRPOS(primary_poc, ' ') - 1) AS first_name,
+         RIGHT(primary_poc, LENGTH(primary_poc) - (STRPOS(primary_poc, ' '))) AS last_name
+  FROM accounts
+)
+
+SELECT first_name, last_name,
+       CONCAT(first_name, '.', last_name, '@', REPLACE(name, ' ', ''), '.com') AS email
+FROM t1;
+/*
+ 3. We would also like to create an initial password, which they will change after their first log in. The first
+   password will be the first letter of the primary_poc's first name (lowercase), then the last letter of their first
+   name (lowercase), the first letter of their last name (lowercase), the last letter of their last name (lowercase),
+   the number of letters in their first name, the number of letters in their last name, and then the name of the
+   company they are working with, all capitalized with no spaces.
+*/
+WITH t1 AS (
+          SELECT name,
+                 LEFT(primary_poc, STRPOS(primary_poc, ' ') - 1) AS first_name,
+                 RIGHT(primary_poc, LENGTH(primary_poc) - (STRPOS(primary_poc, ' '))) AS last_name
+          FROM accounts)
+
+SELECT first_name, last_name,
+       CONCAT(first_name, '.', last_name, '@', name, '.com') AS email, 
+       LEFT(LOWER(first_name), 1) || RIGHT(LOWER(first_name), 1) || LEFT(LOWER(last_name), 1) ||
+          RIGHT(LOWER(last_name), 1) || LENGTH(first_name) || LENGTH(last_name) || REPLACE(UPPER(name), ' ', '') AS password
+FROM t1;
